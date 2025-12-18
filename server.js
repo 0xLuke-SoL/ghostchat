280fee9d2eb9bce81eea6feb9eceff7ab520b78c
Luca Atzeni <macbookduez@MacBookPro.wind3.hub>
2025-12-18 12:12:28 +0100

    Use Render port 10000

--- server.js
+++ server.js
@@ -229,7 +229,7 @@ wss.on('connection', (ws, req) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`GhostChat ONEHOST on :${PORT}`);
 });
